996adcd8a4caa06367a1ad4c74e95bd3f35ffc5f
187qZen <hectepmah@gmail.com>
2026-02-25 15:21:52 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,11 @@
-const CACHE_NAME = 'calc-v2';
-const assets = ['./', './index.html', './manifest.json'];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
-  );
+self.addEventListener('install', () => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
 });
